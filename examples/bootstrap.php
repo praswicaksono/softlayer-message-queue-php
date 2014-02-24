7c4f99c9659e1539f38e73c84f9437d2ebc78f41
Prasetyo Wicaksono <prasetyo@nanomit.es>
2014-02-24 13:04:54 +0700

    invalidate login info

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
 
-define('QUEUE_ACCOUNT', '5yen6'); // Your queue account id.
-define('QUEUE_USERNAME', 'jowy'); // Your portal username.
-define('QUEUE_API_KEY', '013fbe2abacc18f0fe8e74a1520626e3dbbbadbeeeec45a9f40f834cbb5fffc5'); // Your portal API key.
+define('QUEUE_ACCOUNT', 'SOFTLAYER_ACCOUNT_ID'); // Your queue account id.
+define('QUEUE_USERNAME', 'SOFTLAYER_USERNAME'); // Your portal username.
+define('QUEUE_API_KEY', 'SOFTLAYER PORTAL API KEY'); // Your portal API key.
 
 require_once '../vendor/autoload.php';
